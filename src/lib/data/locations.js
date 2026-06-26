8d505ccd24341047b275fae73a590ca1c6ea8626
Markus Klaus <markus.klaus31@gmail.com>
2026-06-26 13:00:02 +0200

    Setup for change to GeoJSON

--- src/lib/data/locations.js
+++ src/lib/data/locations.js
@@ -1,14 +1,20 @@
 export const locations = [
   {
     name: 'Bamberger Dom',
-    lat: 49.8988,
-    lon: 10.9028,
+    lat: 49.890710, 
+    lon: 10.882454,
     description: 'Sehenswürdigkeit'
   },
   {
     name: 'Altes Rathaus',
-    lat: 49.8978,
-    lon: 10.8933,
-    description: 'Historisches Gebäude'
+    lat: 49.891649, 
+    lon: 10.886785,
+    description: 'Sinnbild Bambergs'
+  } ,
+  {
+    name: 'Erba' ,
+    lat: 49.903028,
+    lon: 10.869796,
+    description: 'Ort des schlechten Mittagessens'  
   }
 ];
